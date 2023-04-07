157e4c33135bea539e45fe8ae5b59e6a07af13b2
benjahjp <benjahjp@gmail.com>
2023-04-07 12:28:40 +0300

    feat: Models and mappers

--- app/src/main/java/com/example/spacex/data/remote/dto/CapsuleDtoItem.kt
+++ app/src/main/java/com/example/spacex/data/remote/dto/CapsuleDtoItem.kt
@@ -1,5 +1,7 @@
 package com.example.spacex.data.remote.dto
 
+import com.example.spacex.domain.model.CapsuleModel
+
 data class CapsuleDtoItem(
     val id: String,
     val land_landings: Int,
@@ -11,3 +13,17 @@ data class CapsuleDtoItem(
     val type: String,
     val water_landings: Int
 )
+
+fun CapsuleDtoItem.toCapsuleModel(): CapsuleModel {
+    return CapsuleModel(
+        id = id,
+        landLandings = land_landings,
+        lastUpdate = last_update,
+        launches = launches,
+        reuseCount = reuse_count,
+        serial = serial,
+        status = status,
+        type = type,
+        waterLandings = water_landings
+    )
+}
--- app/src/main/java/com/example/spacex/domain/model/CapsuleModel.kt
+++ app/src/main/java/com/example/spacex/domain/model/CapsuleModel.kt
@@ -1,3 +1,13 @@
 package com.example.spacex.domain.model
 
-data class CapsuleModel()
+data class CapsuleModel(
+    val id: String,
+    val landLandings: Int,
+    val lastUpdate: String,
+    val launches: List<String>,
+    val reuseCount: Int,
+    val serial: String,
+    val status: String,
+    val type: String,
+    val waterLandings: Int
+)
