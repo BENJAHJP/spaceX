cf641e4cbfcb638dc289885a42834b93460cb7b5
benjahjp <benjahjp@gmail.com>
2023-04-07 20:40:25 +0300

    feat: Use case

---
+++ app/src/main/java/com/example/spacex/domain/use_case/GetAllHistoryUseCase.kt
@@ -0,0 +1,29 @@
+package com.example.spacex.domain.use_case
+
+import com.example.spacex.common.Resource
+import com.example.spacex.data.remote.dto.toCoreModel
+import com.example.spacex.data.remote.dto.toHistoryModel
+import com.example.spacex.domain.model.CoreModel
+import com.example.spacex.domain.model.HistoryModel
+import com.example.spacex.domain.repository.SpacexRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetAllHistoryUseCase @Inject constructor(
+    private val repository: SpacexRepository
+) {
+    operator fun invoke(): Flow<Resource<List<HistoryModel>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val history = repository.getAllHistory().map { it.toHistoryModel() }
+            emit(Resource.Success(history))
+        } catch (e: HttpException){
+            emit(Resource.Error("An unknown error occurred"))
+        } catch (e: IOException){
+            emit(Resource.Error("Check your internet connection"))
+        }
+    }
+}
