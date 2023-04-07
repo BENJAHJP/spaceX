a6856b43ab27769efdbbe1a524c507b57480bd4d
benjahjp <benjahjp@gmail.com>
2023-04-07 14:02:07 +0300

    feat: Models and mappers

--- app/src/main/java/com/example/spacex/data/remote/dto/ShipDtoItem.kt
+++ app/src/main/java/com/example/spacex/data/remote/dto/ShipDtoItem.kt
@@ -1,5 +1,7 @@
 package com.example.spacex.data.remote.dto
 
+import com.example.spacex.domain.model.ShipModel
+
 data class ShipDtoItem(
     val abs: Int,
     val active: Boolean,
@@ -26,3 +28,32 @@ data class ShipDtoItem(
     val type: String,
     val year_built: Int
 )
+
+fun ShipDtoItem.toShipModel(): ShipModel{
+    return ShipModel(
+        abs = abs,
+        active = active,
+        classId = `class`,
+        courseDeg = course_deg,
+        homePort = home_port,
+        id = id,
+        image = image,
+        imo = imo,
+        lastAisUpdate = last_ais_update,
+        latitude = latitude,
+        launches = launches,
+        legacyId = legacy_id,
+        link = link,
+        longitude = longitude,
+        massKg = mass_kg,
+        massLbs = mass_lbs,
+        mmsi = mmsi,
+        model = model,
+        name = name,
+        roles = roles,
+        speedKn = speed_kn,
+        status = status,
+        type = type,
+        yearBuilt = year_built
+    )
+}
--- app/src/main/java/com/example/spacex/domain/model/ShipModel.kt
+++ app/src/main/java/com/example/spacex/domain/model/ShipModel.kt
@@ -1,3 +1,28 @@
 package com.example.spacex.domain.model
 
-data class ShipModel()
+data class ShipModel(
+    val abs: Int,
+    val active: Boolean,
+    val classId: Int,
+    val courseDeg: Any,
+    val homePort: String,
+    val id: String,
+    val image: String,
+    val imo: Int,
+    val lastAisUpdate: Any,
+    val latitude: Double,
+    val launches: List<String>,
+    val legacyId: String,
+    val link: String,
+    val longitude: Double,
+    val massKg: Int,
+    val massLbs: Int,
+    val mmsi: Int,
+    val model: String,
+    val name: String,
+    val roles: List<String>,
+    val speedKn: Any,
+    val status: String,
+    val type: String,
+    val yearBuilt: Int
+)
