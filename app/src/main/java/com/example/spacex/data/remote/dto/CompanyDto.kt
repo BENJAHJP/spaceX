058adadd1eb8249b438d9444c430955d02fa7eb9
benjahjp <benjahjp@gmail.com>
2023-04-07 12:36:37 +0300

    feat: Models and mappers

--- app/src/main/java/com/example/spacex/data/remote/dto/CompanyDto.kt
+++ app/src/main/java/com/example/spacex/data/remote/dto/CompanyDto.kt
@@ -1,5 +1,7 @@
 package com.example.spacex.data.remote.dto
 
+import com.example.spacex.domain.model.CompanyModel
+
 data class CompanyDto(
     val ceo: String,
     val coo: String,
@@ -18,3 +20,24 @@ data class CompanyDto(
     val valuation: Long,
     val vehicles: Int
 )
+
+fun CompanyDto.toCompanyModel(): CompanyModel{
+    return CompanyModel(
+        ceo = ceo,
+        coo = coo,
+        cto = cto,
+        ctoPropulsion = cto_propulsion,
+        employees = employees,
+        founded = founded,
+        founder = founder,
+        headquarters = headquarters,
+        id = id,
+        launchSites = launch_sites,
+        links = links,
+        name = name,
+        summary = summary,
+        testSites = test_sites,
+        valuation = valuation,
+        vehicles = vehicles
+    )
+}
--- app/src/main/java/com/example/spacex/domain/model/CompanyModel.kt
+++ app/src/main/java/com/example/spacex/domain/model/CompanyModel.kt
@@ -1,3 +1,23 @@
 package com.example.spacex.domain.model
 
-data class CompanyModel()
+import com.example.spacex.data.remote.dto.Headquarters
+import com.example.spacex.data.remote.dto.LinksX
+
+data class CompanyModel(
+    val ceo: String,
+    val coo: String,
+    val cto: String,
+    val ctoPropulsion: String,
+    val employees: Int,
+    val founded: Int,
+    val founder: String,
+    val headquarters: Headquarters,
+    val id: String,
+    val launchSites: Int,
+    val links: LinksX,
+    val name: String,
+    val summary: String,
+    val testSites: Int,
+    val valuation: Long,
+    val vehicles: Int
+)
