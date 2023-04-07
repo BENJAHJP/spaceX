81300bc7b1fa9040fee89a82720cc410973635d7
benjahjp <benjahjp@gmail.com>
2023-04-07 12:57:09 +0300

    feat: Models and mappers

--- app/src/main/java/com/example/spacex/data/remote/dto/DragonDtoItem.kt
+++ app/src/main/java/com/example/spacex/data/remote/dto/DragonDtoItem.kt
@@ -1,5 +1,7 @@
 package com.example.spacex.data.remote.dto
 
+import com.example.spacex.domain.model.DragonModel
+
 data class DragonDtoItem(
     val active: Boolean,
     val crew_capacity: Int,
@@ -25,3 +27,31 @@ data class DragonDtoItem(
     val type: String,
     val wikipedia: String
 )
+
+fun DragonDtoItem.toDragonModel(): DragonModel{
+    return DragonModel(
+        active = active,
+        crewCapacity = crew_capacity,
+        description = description,
+        diameter = diameter,
+        dryMassKg = dry_mass_kg,
+        dryMassLb = dry_mass_lb,
+        firstFlight = first_flight,
+        flickrImages = flickr_images,
+        heatShield = heat_shield,
+        heightWTrunk = height_w_trunk,
+        id = id,
+        launchPayloadMass = launch_payload_mass,
+        launchPayloadVol = launch_payload_vol,
+        name = name,
+        orbitDurationYr = orbit_duration_yr,
+        pressurizedCapsule = pressurized_capsule,
+        returnPayloadMass = return_payload_mass,
+        returnPayloadVol = return_payload_vol,
+        sidewallAngleDeg = sidewall_angle_deg,
+        thrusters = thrusters,
+        trunk = trunk,
+        type = type,
+        wikipedia = wikipedia
+    )
+}
--- app/src/main/java/com/example/spacex/domain/model/DragonModel.kt
+++ app/src/main/java/com/example/spacex/domain/model/DragonModel.kt
@@ -1,3 +1,38 @@
 package com.example.spacex.domain.model
 
-data class DragonModel()
+import com.example.spacex.data.remote.dto.Diameter
+import com.example.spacex.data.remote.dto.HeatShield
+import com.example.spacex.data.remote.dto.HeightWTrunk
+import com.example.spacex.data.remote.dto.LaunchPayloadMass
+import com.example.spacex.data.remote.dto.LaunchPayloadVol
+import com.example.spacex.data.remote.dto.PressurizedCapsule
+import com.example.spacex.data.remote.dto.ReturnPayloadMass
+import com.example.spacex.data.remote.dto.ReturnPayloadVol
+import com.example.spacex.data.remote.dto.Thruster
+import com.example.spacex.data.remote.dto.Trunk
+
+data class DragonModel(
+    val active: Boolean,
+    val crewCapacity: Int,
+    val description: String,
+    val diameter: Diameter,
+    val dryMassKg: Int,
+    val dryMassLb: Int,
+    val firstFlight: String,
+    val flickrImages: List<String>,
+    val heatShield: HeatShield,
+    val heightWTrunk: HeightWTrunk,
+    val id: String,
+    val launchPayloadMass: LaunchPayloadMass,
+    val launchPayloadVol: LaunchPayloadVol,
+    val name: String,
+    val orbitDurationYr: Int,
+    val pressurizedCapsule: PressurizedCapsule,
+    val returnPayloadMass: ReturnPayloadMass,
+    val returnPayloadVol: ReturnPayloadVol,
+    val sidewallAngleDeg: Int,
+    val thrusters: List<Thruster>,
+    val trunk: Trunk,
+    val type: String,
+    val wikipedia: String
+)
